9335197ff79c0177f59f70b5ef7396c8515d663a
PierrePh@rel <edo.tensei095@gmail.com>
2020-10-22 13:41:37 +0100

    [Lexer] review of Lexer for more clear token and code block support add

--- src/Lexer.rb
+++ src/Lexer.rb
@@ -1,12 +1,16 @@
 #!/usr/bin/env ruby -wKU
-#$LOAD_PATH << '.'
-#require "Common.rb"
+$LOAD_PATH << '.'
+require "Extend.rb"
 
 class Lexer
     attr_reader :tokens
-    BlockBegin = "####"
-    BlockEnd = "###"
-    Keywords = ["@meta", "@title", "@desc", "@brief", "@details", "@param", "@author", "---"]
+    attr_accessor :ark_path
+    Code = ["=begin", "=end"]
+    Keywords = ["@meta", "@title", "@brief", "@param", "@author"]
+
+    def Key(key) Keywords[key].upcase; end
+
+    def Value(key, line) (line.refine)[Keywords[key].size..-1].lstrip; end
 
     def initialize
         @ark_path = "../ark/"
@@ -17,63 +21,83 @@ class Lexer
         files_paths = Dir.new(@ark_path)
 
         files_paths.each_child do |file_path|
-            tmp = File.new(@ark_path + file_path)
-            tmp_tokens = {}.compare_by_identity
-            block = false
-            #code = false
+            lines = file_ary(File.open(@ark_path + file_path, 'r'))
+            file = {}.compare_by_identity
+            i = 0
 
-            tmp.each do |line|
-                chunk = cl_line(line)
-                #cache = {} # must add in tmp_tokens and clean after function proto generation finished
+            # code example
+            code = false
+            code_str = ""
 
-                # block only
-                if chunk.strip == BlockBegin && block == false
-                    block = true
-                elsif chunk.strip == BlockEnd && block == true
-                    block = false
+            while i < lines.size
+
+                # code example token
+                if lines[i].include?(Code[0]) && !code
+                    code = true
+                elsif lines[i].include?(Code[1]) && code
+                    file["CODE"] = code_str
+                    code_str = ""
+                    code = false
                 end
 
-                #puts("chunk 1 :#{chunk}")
-                if block
-                    #puts("chunk 2 :#{chunk}")
-                    chunk = uncomment(chunk)
-                    if identifier = chunk[/\A(@[a-z]\w*)/, 1]
-                        if Keywords.include?(identifier)
-                            #if identifier == KEYWORDS[3]
-                            #    next
-                            #end
+                if code
+                    code_str << lines[i].refine if !lines[i].include?(Code[0])
+                    i += 1
+                    next
+                end
 
-                            id = identifier.lstrip.upcase.delete('@')
-                            value = (chunk[identifier.size..-1]).lstrip
-                            tmp_tokens[id] = value
-                        end
-                    end
+                # keywords tokens
+                if lines[i].include?(Keywords[0]) # meta
+                    file[Key(0)] = Value(0, lines[i])
+                    i += 1
+                elsif lines[i].include?(Keywords[1]) # title (optional)
+                    file[Key(1)] = Value(1, lines[i])
+                    i += 1
+                elsif lines[i].include?(Keywords[2]) # brief
+                    file["FUN"] = fun(lines, i)
+                    file[Key(2)] = Value(2, lines[i])
+                    i += 1
+                elsif lines[i].include?(Keywords[3]) # param
+                    file[Key(3)] = Value(3, lines[i])
+                    i += 1
+                elsif lines[i].include?(Keywords[4]) # author
+                    file[Key(4)] = Value(4, lines[i])
+                    i += 1
+                else
+                    i += 1
                 end
             end
 
-            @tokens << tmp_tokens
+            @tokens << file
         end
     end
 
     private
-    def cl_line(line)
-        cleared_line = line.lstrip
+    def file_ary(file)
+        ary = []
 
-        return cleared_line
+        file.each do |line|
+            ary << line
+        end
+
+        return ary
     end
 
-    def uncomment(line)
-        uncline = line.lstrip.delete('#')
+    def fun(lines, cur_line)
+        i = cur_line
 
-        return uncline
-    end
+        while i < lines.size
+            if lines[i].include?("let") && lines[i].include?("fun") && !lines[i].include?('#')
+                return lines[i]
+            end
 
-    # generate function prototype
-    def prototype_gen(line)
-        return protype
+            i += 1
+        end
+
+        return nil
     end
 end
 
 l = Lexer.new
 l.tokenize
-#puts(l.tokens)
+puts(l.tokens)
