c9fa49cd5467abb6dc8276f16cc1c13a83a9f1c7
PierrePh@rel <edo.tensei095@gmail.com>
2020-11-21 16:49:06 +0100

    [Lexer] block doc delimiter adding

--- src/Lexer.rb
+++ src/Lexer.rb
@@ -6,6 +6,7 @@ require "Format.rb"
 class Lexer
     attr_reader :tokens
     attr_accessor :ark_path
+    DocBlock = ["###", "##"]
     Code = ["=begin", "=end"]
     Keywords = ["@meta", "@brief", "@param", "@author", "@details"]
 
@@ -26,57 +27,71 @@ class Lexer
             file = {}.compare_by_identity
             i = 0
 
+            # doc block
+            doc_block = false
             # code example
             code = false
             code_str = ""
 
             while i < lines.size
-
-                # code example token
-                if lines[i].include?(Code[0]) && !code
-                    code = true
-                elsif lines[i].include?(Code[1]) && code
-                    file["CODE"] = code_str
-                    code_str = ""
-                    code = false
+                if lines[i].strip == DocBlock[0] && !doc_block
+                    puts("begin : #{i + 1}")
+                    doc_block = true
+                elsif lines[i].strip == DocBlock[1] && doc_block
+                    puts("end : #{i + 1}")
+                    doc_block = false
                 end
 
-                if code
-                    code_str << lines[i].refine if !lines[i].include?(Code[0])
-                    i += 1
-                    next
-                end
-
-                # keywords tokens
-                if lines[i].include?(Keywords[0]) # meta
-                    if (has_desc = Value(0, lines[i]).index(" ")) != nil
-                        meta = Value(0, lines[i]).insert(has_desc, "-").split("-")
-                        # for now support only titles and pages overview
-                        meta[0] = (meta[0] << NewLine) # title
-                        meta[1] = meta[1].lstrip
-                        file["TITLE"] = meta[0]
-                        file["OVERVIEW"] = meta[1]
-                    else
-                        meta = Value(0, lines[i])
-                        file["TITLE"] = meta
+                if doc_block
+                    # code example token
+                    if lines[i].include?(Code[0]) && !code
+                        code = true
+                    elsif lines[i].include?(Code[1]) && code
+                        file["CODE"] = code_str
+                        code_str = ""
+                        code = false
                     end
-                    i += 1
-                elsif lines[i].include?(Keywords[1]) # brief
-                    file["FUN"] = fun(lines, i)
-                    file[Key(1)] = Value(1, lines[i])
-                    i += 1
-                elsif lines[i].include?(Keywords[2]) # param
-                    file[Key(2)] = Value(2, lines[i])
-                    i += 1
-                elsif lines[i].include?(Keywords[3]) # author
-                    file[Key(3)] = Value(3, lines[i])
-                    i += 1
-                elsif lines[i].include?(Keywords[4]) # details
-                    file[Key(4)] = Value(4, lines[i])
-                    i += 1
+
+                    if code
+                        code_str << lines[i].refine if !lines[i].include?(Code[0])
+                        i += 1
+                        next
+                    end
+
+                    # keywords tokens
+                    if lines[i].include?(Keywords[0]) # meta
+                        if (has_desc = Value(0, lines[i]).index(" ")) != nil
+                            meta = Value(0, lines[i]).insert(has_desc, "-").split("-")
+                            # for now support only titles and pages overview
+                            meta[0] = (meta[0] << NewLine) # title
+                            meta[1] = meta[1].lstrip
+                            file["TITLE"] = meta[0]
+                            file["OVERVIEW"] = meta[1]
+                        else
+                            meta = Value(0, lines[i])
+                            file["TITLE"] = meta
+                        end
+                        i += 1
+                    elsif lines[i].include?(Keywords[1]) # brief
+                        file["FUN"] = fun(lines, i)
+                        file[Key(1)] = Value(1, lines[i])
+                        i += 1
+                    elsif lines[i].include?(Keywords[2]) # param
+                        file[Key(2)] = Value(2, lines[i])
+                        i += 1
+                    elsif lines[i].include?(Keywords[3]) # author
+                        file[Key(3)] = Value(3, lines[i])
+                        i += 1
+                    elsif lines[i].include?(Keywords[4]) # details
+                        file[Key(4)] = Value(4, lines[i])
+                        i += 1
+                    else
+                        i += 1
+                    end
                 else
                     i += 1
                 end
+
             end
 
             @tokens << file
