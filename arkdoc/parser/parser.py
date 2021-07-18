ba6f5996961d064e9ff51bd6cb0dd1a08e7ed1ea
Alexandre Plateau <lexplt.dev@gmail.com>
2021-07-18 16:13:53 +0200

    adding documentation extraction

--- arkdoc/parser/parser.py
+++ arkdoc/parser/parser.py
@@ -22,8 +22,10 @@ class Documentation:
     def _token_format(self, token: Token):
         return token.value
 
-    def extract_signature(self, on: List[Token] = None):
+    def _signature(self, on: List[Token] = None):
+        transform = lambda L: list(map(lambda t: t.value, deep_flatten(L)))
         top = self.target[:] if on is None else on
+
         while True:
             if isinstance(top, list) and \
                     len(top) and isinstance(top[0], list):
@@ -31,15 +33,22 @@ class Documentation:
             else:
                 break
 
-        transform = lambda L: list(map(lambda t: t.value, deep_flatten(L)))
-
         if on is None:
-            return transform(top[:2]) + self.extract_signature(on=top[2])
+            return transform(top[:2]) + self._signature(on=top[2])
         else:
             return transform(top[1:2])
 
+    @property
+    def function_signature(self):
+        kw, name, *args = self._signature()
+        return f"({kw} {name} (fun ({' '.join(args)}) (...)))"
+
+    @property
+    def defined_at(self):
+        return deep_flatten(self.target[:])[0].line
+
     def __str__(self):
-        return f"Documentation({len(self.comments)} comments, {self.extract_signature()})"
+        return f"Documentation({len(self.comments)} comments, {self.function_signature}, line={self.defined_at})"
 
 
 class Parser:
@@ -59,30 +68,34 @@ class Parser:
             r'^#+ *@\w+', token.value
         ) is not None
 
-    def extract_doc(self, node: List):
-        doc = None
+    def _doc_extractor(self, node: List):
+        comments = None
 
         for n in node:
             if not isinstance(n, list):
                 if self._is_doc_comment(n):
-                    if doc is None:
-                        doc = Documentation([], None)
-                    doc.comments.append(n)
-            elif doc is None:
-                self.extract_doc(n)
+                    comments = [n] if comments is None else comments + [n]
             else:
-                doc.target = n
-                yield doc
-                doc = None
+                if comments is None:
+                    yield from self._doc_extractor(n)
+                elif comments is not None:
+                    yield Documentation(comments, n)
+                    comments = None
+                    yield from self._doc_extractor(n)
 
-                yield from self.extract_doc(n)
+    def extract_documentation(self):
+        yield from self._doc_extractor(self.ast)
 
     def parse(self):
         with open(self.filename, 'r') as f:
             program = f.read()
 
-        self.ast = tree_from_tokens(list(tokenize(program)))
+        self.ast = []
+        tokens = list(tokenize(program))
+
+        while tokens:
+            self.ast += tree_from_tokens(tokens)
         # self.visit(self.ast)
 
-        for e in self.extract_doc(self.ast):
+        for e in self.extract_documentation():
             logger.debug(e)
