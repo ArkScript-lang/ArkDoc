c72680719e50b45f486034fde5e790f6bb54f360
Alexandre Plateau <lexplt.dev@gmail.com>
2021-07-18 12:41:03 +0200

    adding a basic logger

---
+++ arkdoc/logger.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import colorama
+from enum import Enum
+from datetime import datetime
+
+
+class LogLevel(Enum):
+    DEBUG = 4
+    INFO = 3
+    WARN = 2
+    ERROR = 1
+    NONE = 0
+
+
+class Logger:
+    def __init__(self, name: str, level: LogLevel = LogLevel.NONE):
+        self.name = name
+        self.level = level
+
+    def _print(self, kind: LogLevel, *args):
+        if kind.value > self.level:
+            return
+
+        colors = {
+            LogLevel.DEBUG: colorama.Fore.MAGENTA,
+            LogLevel.INFO: colorama.Fore.CYAN,
+            LogLevel.WARN: colorama.Fore.YELLOW,
+            LogLevel.ERROR: colorama.Fore.RED
+        }
+        now = datetime.now().isoformat(timespec='minutes')
+        print(f"[${colors[kind]}{kind.name}{colorama.Fore.RESET}] -- {now} -- {' '.join(args)}")
+
+    def debug(self, *args):
+        self._print(LogLevel.DEBUG, *args)
+
+    def info(self, *args):
+        self._print(LogLevel.INFO, *args)
+
+    def warn(self, *args):
+        self._print(LogLevel.WARN, *args)
+
+    def error(self, *args):
+        self._print(LogLevel.ERROR, *args)
+
+
+colorama.init()
+logger = Logger('ArkDoc', level=LogLevel.DEBUG)
