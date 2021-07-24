f6099087ccdb74075cb4a9222a30aac25bb7ce3f
Alexandre Plateau <lexplt.dev@gmail.com>
2021-07-24 18:30:05 +0200

    starting the generation of the html documentation

--- arkdoc/generator/base.py
+++ arkdoc/generator/base.py
@@ -12,6 +12,7 @@ from ..parser import Parser
 
 class Generator:
     def __init__(self, parsers: List[Parser], template_folder: Path, pattern: str):
+        self.template_folder = template_folder
         self.templates = {
             file.name: file.read_text("utf-8")
             for file in template_folder.glob(pattern)
@@ -34,12 +35,16 @@ class Generator:
 
             self.list.files.append(file)
 
-    def _generate(self, path: str, functions: List[spec.Function]):
+    def generate_index(self):
+        raise NotImplementedError
+
+    def generate_one(self, path: str, functions: List[spec.Function]):
         raise NotImplementedError
 
     def __call__(self):
-        for file in self.list.files:
-            logger.info(f"Generating {file.path} documentation...")
-            logger.info(f"\tFound {len(file.functions)} functions")
+        self.generate_index()
 
-            self._generate(file.path, file.functions)
+        for file in self.list.files:
+            logger.info(f"Generating {file.path} documentation... found {len(file.functions)} functions")
+
+            self.generate_one(file.path, file.functions)
--- arkdoc/generator/html.py
+++ arkdoc/generator/html.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import re
+from datetime import datetime
+import shutil
 from typing import List
 from pprint import pformat
 from pathlib import Path
@@ -14,11 +16,13 @@ from ..parser import Parser
 class HTMLGenerator(Generator):
     def __init__(self, parsers: List[Parser], output: str):
         super().__init__(parsers, spec.HTML_TEMPLATE_FOLDER, "*.html")
-        self.output = Path(output)
+        self.output_path = Path(output)
         self.fields = {
             name: re.findall(r"{\w+}", content, re.MULTILINE)
             for name, content in self.templates.items()
         }
+
+        self.footer = f"Last generation at {datetime.now()}"
 
     def make_nav_link(self, name: str, path: str) -> str:
         return f"<a href=\"{path}\" class=\"btn btn-link\">{name}</a>"
@@ -39,7 +43,31 @@ class HTMLGenerator(Generator):
     </div>
 </section>"""
 
-    def _generate(self, path: str, functions: List[spec.Function]):
-        logger.info("---- ", path)
-        logger.info(pformat(functions, indent=4))
+    def create_dir(self, name: str):
+        (self.output_path / name).mkdir()
+
+    def generate_index(self):
+        if not self.output_path.exists():
+            shutil.copytree(str(self.template_folder / "assets"), str(self.output_path / "assets"))
+        else:
+            shutil.rmtree(str(self.output_path))
+            if self.output_path.exists():
+                self.output_path.rmdir()
+            return self.generate_index()
+
+        sections = self.make_section("Test", "test")
+
+        content = self.templates["index.html"]
+        content = content.format(
+            page_title="ArkScript documentation",
+            banner=self.templates["banner.html"],
+            navigation_links="",
+            sections=sections,
+            footer=self.footer
+        )
+
+        (self.output_path / "index.html").write_text(content)
+
+    def generate_one(self, path: str, functions: List[spec.Function]):
+        local_fields = self.fields.copy()
         return None
